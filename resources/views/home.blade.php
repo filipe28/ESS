fe5b1fdf68f98bf171167da744c278be728eb96d
filipe28 <2171302@my.ipleiria.pt>
2020-05-16 23:43:37 +0100

    x

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Dashboard' )
 @section('content')
 
-s<div id="temps_div"></div>
+<div id="temps_div"></div>
 // With Lava class alias
 <?= Lava::render('LineChart', 'Temps', 'temps_div') ?>
 
